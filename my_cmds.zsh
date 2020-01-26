d869b6474f67cfa8d43ce7b272ad165368503491
zno <zno@iMac.local>
2020-01-26 16:09:49 +0900

    added a function that changes a file name encoding

--- my_cmds.zsh
+++ my_cmds.zsh
@@ -35,3 +35,19 @@ chfenc "*.smi" cp949 utf8
 function chfenc () {
     find . -type f -name $1 -exec sh -c 'iconv -f '$2' -t '$3' "$0" > "$0.tmp"' '{}' \; -exec mv -f '{}.tmp' '{}' \;
 }
+
+# change file name encoding
+# usage : chnenc path from_enc to_enc
+function chnenc () {
+    find $1 ! -path . -depth -exec sh -c 'mv "{}" "{}_tmp" ; echo {} | iconv -f '$2' -t '$3 \; | xargs -I% mv '%_tmp' '%'
+}
+
+# change file name encoding to NFC
+# usage : chn2NFC path
+# example for a current dir : chn2NFC .
+# example for a specific dir (include dir) : chn2NFC dir
+# example for a specific dir (exclude dir) : chn2NFC dir/*
+# example for a specific file : chn2NFC file
+function chn2NFC () {
+    chnenc $1 UTF8-MAC UTF8
+}
