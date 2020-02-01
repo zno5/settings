05da82c829f68248ddbe9d2966483a32b0aaf9dc
zno <hesed@live.com>
2020-02-01 16:25:38 +0900

    use local var in my_cmds.zsh

--- my_cmds.zsh
+++ my_cmds.zsh
@@ -13,13 +13,13 @@ function buildcpp () {
 
 # Use lf to switch directories and bind it to ctrl-o
 function lfcd () {
-    tmp="$(mktemp)"
+    local tmp="$(mktemp)"
     lf -last-dir-path="$tmp" "$@"
-if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
+    if [ -f "$tmp" ]; then
+        local dir="$(cat "$tmp")"
         rm -f "$tmp"
         [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
-fi
+    fi
 }
 bindkey -s '^o' 'lfcd\n'
 
